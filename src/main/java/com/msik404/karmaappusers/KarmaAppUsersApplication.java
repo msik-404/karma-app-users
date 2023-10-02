c475c9c666c73ecd2b3e45c087ec4407d1e878ac
SIIIKOR <siiikor.sikorski@gmail.com>
2023-10-02 15:48:46 +0200

    initial commit with spring init

---
+++ src/main/java/com/msik404/karmaappusers/KarmaAppUsersApplication.java
@@ -0,0 +1,13 @@
+package com.msik404.karmaappusers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KarmaAppUsersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KarmaAppUsersApplication.class, args);
+	}
+
+}

6465897295b46ce59a1d8f8ebea0a7d2e055306d
SIIIKOR <siiikor.sikorski@gmail.com>
2023-10-06 14:34:13 +0200

    class transformed to record

--- src/main/java/com/msik404/karmaappusers/user/dto/UserDto.java
+++ src/main/java/com/msik404/karmaappusers/user/dto/UserDto.java
@@ -1,19 +1,7 @@
 package com.msik404.karmaappusers.user.dto;
 
 import com.msik404.karmaappusers.user.Role;
-import lombok.Getter;
 import org.bson.types.ObjectId;
 
-@Getter
-public class UserDto {
-
-    private ObjectId userId;
-
-    private String firstName;
-    private String lastName;
-    private String username;
-    private String email;
-    private String password;
-    private Role role;
-
+public record UserDto(ObjectId userId, String firstName, String lastName, String username, String email, String password, Role role) {
 }

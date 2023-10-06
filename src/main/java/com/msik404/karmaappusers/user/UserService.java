0db961ba76d2ac3322d677eaf5b35793f199ccb3
SIIIKOR <siiikor.sikorski@gmail.com>
2023-10-06 14:48:04 +0200

    Wrote methods for getting hashed password and role of given user. These methods throw exceptions if requested data is not found.

--- src/main/java/com/msik404/karmaappusers/user/UserService.java
+++ src/main/java/com/msik404/karmaappusers/user/UserService.java
@@ -1,6 +1,10 @@
 package com.msik404.karmaappusers.user;
 
+import java.util.Optional;
+
 import com.mongodb.client.result.UpdateResult;
+import com.msik404.karmaappusers.user.dto.PasswordOnlyDto;
+import com.msik404.karmaappusers.user.dto.RoleOnlyDto;
 import com.msik404.karmaappusers.user.dto.UserDto;
 import com.msik404.karmaappusers.user.exception.DuplicateEmailException;
 import com.msik404.karmaappusers.user.exception.DuplicateUnexpectedFieldException;
@@ -8,6 +12,7 @@ import com.msik404.karmaappusers.user.exception.DuplicateUsernameException;
 import com.msik404.karmaappusers.user.exception.UserDocumentNotFoundException;
 import com.msik404.karmaappusers.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.bson.types.ObjectId;
 import org.springframework.dao.DuplicateKeyException;
 import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Service;
@@ -40,4 +45,23 @@ public class UserService {
             DuplicateKeyExceptionHandler.handle(ex);
         }
     }
+
+    public String findHashedPassword(@NonNull String email) throws UserDocumentNotFoundException {
+
+        final Optional<PasswordOnlyDto> optionalPasswordDto = repository.findByEmail(email);
+        if (optionalPasswordDto.isEmpty()) {
+            throw new UserDocumentNotFoundException();
+        }
+        return optionalPasswordDto.get().password();
+    }
+
+    public Role findRole(@NonNull ObjectId userId) throws UserDocumentNotFoundException {
+
+        final Optional<RoleOnlyDto> optionalRoleDto = repository.findByUserId(userId);
+        if (optionalRoleDto.isEmpty()) {
+            throw new UserDocumentNotFoundException();
+        }
+        return optionalRoleDto.get().role();
+    }
+
 }

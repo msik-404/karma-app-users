8d522f3782a2931811ed175691373820a1c59623
SIIIKOR <siiikor.sikorski@gmail.com>
2023-10-06 23:32:43 +0200

    Added docker scripts

---
+++ scripts/clean.sh
@@ -0,0 +1,17 @@
+#! /usr/bin/bash
+
+# stop containers
+DIR=$(dirname $BASH_SOURCE)
+source "$DIR/stop.sh"
+
+# delete containers
+docker rm karma-app-users-backend-1 && docker rm karma-app-users-mongo-express-1 && docker rm karma-app-users-mongo-1
+
+# deleted backend image
+docker image rm karma-app-users-backend
+
+# delete volumes
+docker volume rm karma-app-users_db-config && docker volume rm karma-app-users_db-data
+
+# delete network
+docker network rm karma-app-users_karma-app-net
---
+++ scripts/stop.sh
@@ -0,0 +1,3 @@
+#! /usr/bin/bash
+
+docker stop karma-app-users-backend-1 && docker stop karma-app-users-mongo-express-1 && docker stop karma-app-users-mongo-1

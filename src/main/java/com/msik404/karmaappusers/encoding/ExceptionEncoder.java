60cbca35da76282aed354d277dfc07d4e9a31a67
SIIIKOR <siiikor.sikorski@gmail.com>
2023-10-18 18:50:17 +0200

    Exception id prefix now contains whitelist. By doing so, it is easier to change encoding and decoding process.

--- src/main/java/com/msik404/karmaappusers/encoding/ExceptionEncoder.java
+++ src/main/java/com/msik404/karmaappusers/encoding/ExceptionEncoder.java
@@ -4,11 +4,11 @@ import org.springframework.lang.NonNull;
 
 public class ExceptionEncoder {
 
-    public final static String EXCEPTION_ID_PREFIX = "exceptionId:";
+    public final static String EXCEPTION_ID_PREFIX = "exceptionId: ";
 
     @NonNull
     public static String encode(@NonNull final String exceptionIdString, @NonNull final String errorMessage) {
-        return String.format("%s %s %s", EXCEPTION_ID_PREFIX, exceptionIdString, errorMessage);
+        return String.format("%s%s %s", EXCEPTION_ID_PREFIX, exceptionIdString, errorMessage);
     }
 
 }
